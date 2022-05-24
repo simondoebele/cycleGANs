0cbeee40ef72a8c848f311a2567ba086e9d661cf
Frankkie <christosfrantzolas@gmail.com>
2022-05-24 16:22:31 +0200

    Segmentation done!

---
+++ images4segmentation/README.md
@@ -0,0 +1,1 @@
+Place your results from the CycleGAN here!
--- src/segmentation.py
+++ src/segmentation.py
@@ -1,13 +1,65 @@
 import torch
+from PIL import Image
+from torchvision import transforms
+import os
+from load_data import pil_loader
+from device import DEVICE
+import matplotlib.pyplot as plt
+import numpy as np
+from torchvision.utils import save_image
+
+path = "images4segmentation"
+
+
+def load_images(path, images):
+    original = []
+    stylized = []
+    images.sort()
+    for img in images:
+        img_path = os.path.join(path, img)
+        if "X_real" in img:
+            loaded = pil_loader(img_path)
+            original.append(loaded)
+        elif "Y_fake" in img:
+            loaded = pil_loader(img_path)
+            stylized.append(loaded)
+
+    return original, stylized
+
+
+def transform(image, trans, device):
+    transformed = trans(image)
+    input_batch = transformed.unsqueeze(0)
+    input_batch = input_batch.to(device)
+    return input_batch
+
+
+images = os.listdir(path)
+
+preprocess = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+to_tensor = transforms.ToTensor()
+
+original, stylized = load_images(path, images)
 segm_model = torch.hub.load('pytorch/vision:v0.10.0', 
-                            'deeplabv3_resnet50', pretrained=True)
+                            'deeplabv3_resnet50', pretrained=True).to(DEVICE)
 segm_model.eval()
 
 
-"""All pre-trained models expect input images normalized in the same way, 
-i.e. mini-batches of 3-channel RGB images of shape (N, 3, H, W), 
-where N is the number of images, H and W are expected to be at least 224 pixels. 
-The images have to be loaded in to a range of [0, 1] and then normalized using 
-mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225]. 
-output['out'] is of shape (N, 21, H, W)"""
+for i, image in enumerate(original):
+    output = segm_model(transform(image, preprocess, DEVICE))['out'][0]
+    output_pred = output.argmax(0).unsqueeze(0).cpu().numpy()
+    output_pred = np.where(output_pred != 15, 0, output_pred)
+    mask = np.where(output_pred == 15, 1, output_pred).reshape((256, 256))
+    mask = np.stack([mask, mask, mask])
+
+    masked_stylized = mask * to_tensor(stylized[i]).numpy().reshape((3, 256, 256))
+    combined = (1 - mask) * to_tensor(image).numpy().reshape((3, 256, 256)) + masked_stylized
+    combined = torch.Tensor(combined)
+
+    save_image(combined, f"combined/{i}.png")
+
 
